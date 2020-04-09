9ead8cd8361cce7102a1de3344def4bb68135c0b
Joan Ciprià <joancipria@gmail.com>
2020-04-09 20:59:15 +0200

    Some cleaning

--- src/renderer/views/Dashboard.vue
+++ src/renderer/views/Dashboard.vue
@@ -2,26 +2,13 @@
   <div id="wrapper">
     <main>
       <div>
-        <h3>ROS Connection</h3>
-        <hr />
-        <label>ROSBridge address</label>
-        <input type="text" v-model="rosbridge_address" />
         <button v-if="connected" @click="disconnect">Connected!</button>
         <button v-else @click="connect">Connect!</button>
-      </div>
-      <div>
-        <button :disabled="!connected" @click="sendCommand">Move the Robot!</button>
-        <button :disabled="!connected" @click="sendTurnRight">Turn right!</button>
         <button :disabled="!connected" @click="sendStop">Stop the Robot!</button>
+        <span>X: {{ position.x.toFixed(2) }}</span>
+        <span>Y: {{ position.y.toFixed(2) }}</span>
+        <span>Z: {{ position.z.toFixed(2) }}</span>
         <hr />
-        <p>Subscribing robot data</p>
-        <p>
-          <span>X: {{ position.x.toFixed(2) }}</span>
-          <br />
-          <span>Y: {{ position.y.toFixed(2) }}</span>
-          <br />
-          <span>Z: {{ position.z.toFixed(2) }}</span>
-        </p>
       </div>
       <LiveMap></LiveMap>
     </main>
@@ -46,31 +33,6 @@ export default {
     connect: function() {
       this.connectRos()
     },
-    sendCommand: function() {
-      let topic = new ROSLIB.Topic({
-        ros: this.ros,
-        name: "/cmd_vel",
-        messageType: "geometry_msgs/Twist"
-      });
-      let message = new ROSLIB.Message({
-        linear: { x: 1, y: 0, z: 0 },
-        angular: { x: 0, y: 0, z: 0.5 }
-      });
-      topic.publish(message);
-    },
-
-    sendTurnRight: function() {
-      let topic = new ROSLIB.Topic({
-        ros: this.ros,
-        name: "/cmd_vel",
-        messageType: "geometry_msgs/Twist"
-      });
-      let message = new ROSLIB.Message({
-        linear: { x: -1, y: 0, z: 0 },
-        angular: { x: 0, y: 0, z: 0.5 }
-      });
-      topic.publish(message);
-    },
     sendStop: function() {
       let topic = new ROSLIB.Topic({
         ros: this.ros,
@@ -82,9 +44,6 @@ export default {
         angular: { x: 0, y: 0, z: 0 }
       });
       topic.publish(message);
-    },
-    open(link) {
-      this.$electron.shell.openExternal(link);
     }
   }
 };
