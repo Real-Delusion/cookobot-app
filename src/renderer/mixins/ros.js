6f261af9a8301fd0aebc4fbe33c1716693dfa0b0
Joan Ciprià <joancipria@gmail.com>
2020-04-09 20:40:33 +0200

    Create Ros mixin

---
+++ src/renderer/mixins/ros.js
@@ -0,0 +1,47 @@
+export default {
+    data() {
+        return {
+            // ros connection
+            ros: null,
+            rosbridge_address: "ws://127.0.0.1:9090/",
+            connected: false,
+        }
+    },
+    created: function () {
+        this.connect()
+    },
+    methods: {
+        connect: function () {
+            // define ROSBridge connection object
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            });
+
+            // define callbacks
+            this.ros.on("connection", () => {
+                this.connected = true;
+                console.log("Connection to ROSBridge established!");
+                let topic = new ROSLIB.Topic({
+                    ros: this.ros,
+                    name: "/odom",
+                    messageType: "nav_msgs/Odometry"
+                });
+                topic.subscribe(message => {
+                    this.position = message.pose.pose.position;
+                    //console.log(message);
+                });
+            });
+            this.ros.on("error", error => {
+                console.log("Something went wrong when trying to connect");
+                console.log(error);
+            });
+            this.ros.on("close", () => {
+                this.connected = false;
+                console.log("Connection to ROSBridge was closed!");
+            });
+        },
+        disconnect: function () {
+            this.ros.close();
+        }
+    }
+}
