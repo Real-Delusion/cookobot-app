d86b2c1fd0ed33788a92eadb8415c8bc62ebca0c
Joan Ciprià <joancipria@gmail.com>
2020-04-10 19:38:54 +0200

    Make connect method async

--- src/renderer/mixins/ros.js
+++ src/renderer/mixins/ros.js
@@ -13,32 +13,35 @@ export default {
     },
     methods: {
         connectRos: function () {
-            // define ROSBridge connection object
-            this.ros = new ROSLIB.Ros({
-                url: this.rosbridge_address
-            });
+            return new Promise((resolve, reject) => {
+                // define ROSBridge connection object
+                this.ros = new ROSLIB.Ros({
+                    url: this.rosbridge_address
+                });
 
-            // define callbacks
-            this.ros.on("connection", () => {
-                this.connected = true;
-                console.log("Connection to ROSBridge established!");
-                let topic = new ROSLIB.Topic({
-                    ros: this.ros,
-                    name: "/odom",
-                    messageType: "nav_msgs/Odometry"
+                // define callbacks
+                this.ros.on("connection", () => {
+                    this.connected = true;
+                    console.log("Connection to ROSBridge established!");
+                    let topic = new ROSLIB.Topic({
+                        ros: this.ros,
+                        name: "/odom",
+                        messageType: "nav_msgs/Odometry"
+                    });
+                    topic.subscribe(message => {
+                        this.position = message.pose.pose.position;
+                        //console.log(message);
+                    });
                 });
-                topic.subscribe(message => {
-                    this.position = message.pose.pose.position;
-                    //console.log(message);
+                this.ros.on("error", error => {
+                    console.log("Something went wrong when trying to connect");
+                    console.log(error);
                 });
-            });
-            this.ros.on("error", error => {
-                console.log("Something went wrong when trying to connect");
-                console.log(error);
-            });
-            this.ros.on("close", () => {
-                this.connected = false;
-                console.log("Connection to ROSBridge was closed!");
+                this.ros.on("close", () => {
+                    this.connected = false;
+                    console.log("Connection to ROSBridge was closed!");
+                });
+                return resolve(0);
             });
         },
         disconnect: function () {
