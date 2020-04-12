b1ec8355f20dd41c1de8013456627c0e68b959cd
Joan Ciprià <joancipria@gmail.com>
2020-04-12 22:39:17 +0200

    Import bootstrap

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,10 +4,17 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+// Import bootstrap
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+// Vue use bootstrap
+Vue.use(BootstrapVue)
 
 export const bus = new Vue();
 
