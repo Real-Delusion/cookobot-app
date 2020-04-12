7b7cad9105132a88f82e73d56e6def8fd69b1f7f
Joan Ciprià <joancipria@gmail.com>
2020-04-12 14:59:33 +0200

    Implement getUserById()

--- src/renderer/utils/db.js
+++ src/renderer/utils/db.js
@@ -1,18 +1,21 @@
 const sqlite3 = require('sqlite3').verbose()
 let db
 
+// Connect to db file
 function connect() {
     if (!db || !db.open) {
-        db = new sqlite3.Database('base.db')
+        db = new sqlite3.Database('database.db')
     }
-    console.log(db)
     return db
 }
 
-export const getUserById = () => {
+// Get user by id
+export const getUserById = (id) => {
     return new Promise((resolve, reject) => {
         let db = connect()
-        db.all('select * from users', (err, rows) => {
+        let query = `select * from users where user_id = ?`;
+
+        db.get(query, [id], (err, rows) => {
             if (err) reject(err)
             resolve(rows || [])
         })
