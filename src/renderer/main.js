b5e89438b342fd5b57707357d4661484a18b9c21
Santiago <sanpetor@epsg.upv.es>
2020-04-13 19:35:52 +0200

    imported fontawesome

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,15 @@ import App from './App'
 import router from './router'
 import store from './store'
 import 'bulma/css/bulma.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+ 
+library.add(faCog)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+ 
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
