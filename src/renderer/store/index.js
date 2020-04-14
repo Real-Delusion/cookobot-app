82f297cf5e5dca853e685661ce7cd91d234f24aa
Joan Ciprià <joancipria@gmail.com>
2020-04-14 12:53:05 +0200

    Store user data with vuex store

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -7,16 +7,40 @@ import modules from './modules'
 
 Vue.use(Vuex)
 
+// Vuex Store
 export default new Vuex.Store({
   modules,
   plugins: [
-    createPersistedState(),
-    createSharedMutations()
+    createPersistedState()
+    //createSharedMutations()
   ],
+  state: {
+    auth: false, // authenticated
+    user_name: "", // user name
+    user_type: 0 // user type ( 0 --> User, 1 --> Admin)
+  },
+  mutations: {
+    auth(state, status) {
+      state.auth = status;
+    },
+    name(state, name) {
+      state.user_name = name;
+    },
+    type(state, type) {
+      state.user_type = type;
+    }
+  },
+  actions: {
+    auth(context, status) {
+      context.commit('auth', status)
+    },
+    name(context, status) {
+      context.commit('name', status)
+    },
+    type(context, status) {
+      context.commit('type', status)
+    }
+  },
   strict: process.env.NODE_ENV !== 'production'
 })
 
-Vue.prototype.$userData = {
-  user_name: "",
-  user_type: 0
-}
