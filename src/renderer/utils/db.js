cf958bc9a1f93e0b64e4fc157d519d62ac7c78c3
Joan Ciprià <joancipria@gmail.com>
2020-04-15 11:39:07 +0200

    Create db on appData path. Added initDataBase function

--- src/renderer/utils/db.js
+++ src/renderer/utils/db.js
@@ -1,10 +1,40 @@
+import path from 'path'
+import fs from 'fs'
+import { remote } from 'electron'
 const sqlite3 = require('sqlite3').verbose()
+
 let db
+const dbPath = path.join(remote.app.getPath('appData'), '/database.db');
+
+// Check for db file
+try {
+    if (!fs.existsSync(dbPath)) {
+        // Create if does not exist
+        initDataBase()
+    }
+} catch (err) {
+    console.error(err)
+}
+
+// Init database
+function initDataBase() {
+    let db = connect()
+    db.serialize(() => {
+        db.run(`CREATE TABLE "users" (
+            "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+            "user_id"	TEXT NOT NULL,
+            "name"	TEXT NOT NULL,
+            "user_type"	INTEGER NOT NULL
+        )`)
+        db.run(`INSERT INTO "main"."users" ("id", "user_id", "name", "user_type") VALUES ('1', '0000', 'Admin', '1')`)
+        db.run(`INSERT INTO "main"."users" ("id", "user_id", "name", "user_type") VALUES ('2', '1234', 'User', '0')`)
+    })
+}
 
 // Connect to db file
 function connect() {
     if (!db || !db.open) {
-        db = new sqlite3.Database('database.db')
+        db = new sqlite3.Database(dbPath)
     }
     return db
 }
