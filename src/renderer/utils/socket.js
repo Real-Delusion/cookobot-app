97c51cb86629f0772815614085b9b0ea4e640711
Joan Ciprià <joancipria@gmail.com>
2020-04-15 18:38:04 +0200

    Create socket server

---
+++ src/renderer/utils/socket.js
@@ -0,0 +1,23 @@
+/*------------------------------------
+---------- Socket.io Server ----------
+-------------------------------------*/
+
+// Import socket.io
+const io = require('socket.io')();
+
+// On client connected
+io.on('connection', client => {
+    //console.log("Client connected")
+
+    // Receive issue event
+    client.on('issue', (msg) => {
+        console.log('Issue received: ' + msg);
+    });
+});
+
+// Listen on port 3000
+io.listen(3000);
+
+export default {
+    io
+}
