a051349b2ae393aa1a0f3d4682fcc96b4a77a3cc
almiab1 <almiab1@epsg.upv.es>
2020-04-20 02:31:50 +0200

    add style error message

--- src/renderer/views/Login.vue
+++ src/renderer/views/Login.vue
@@ -11,17 +11,21 @@
 
         <div class="column is-one-third is-centered">
           <div class="column is-full" style="height: 50%">
-            <p class="message">{{message}}</p>
+            <div v-if="errorMessage == true" class="notification is-danger">
+              <button class="delete" @click="hideError()"></button>
+              <strong>{{message}}</strong>
+            </div>
+           
           </div>
           <div class="column is-full is-centered" id="loginInputContainer">
             <h1 class="title is-1" id="inputLogIn">{{userid}}</h1>
           </div>
-          
+
         </div>
-        
+
       </div>
     </div>
-    
+
     <div class="columns is-marginless is-centered">
       <div class="column is-one-third" style="height: 75vh">
         <NumericKeyboard @pressed="userid = $event" :selfValue="userid"></NumericKeyboard>
@@ -31,94 +35,101 @@
 </template>
 
 <script>
-import { getUserById } from "@/utils/db";
-import NumericKeyboard from "@/components/UI/NumericKeyboard/NumericKeyboard";
+  import {
+    getUserById
+  } from "@/utils/db";
+  import NumericKeyboard from "@/components/UI/NumericKeyboard/NumericKeyboard";
 
-export default {
-  name: "login",
-  components: {
-    NumericKeyboard
-  },
-  data() {
-    return {
-      // input: {
-      //   userid: ""
-      // },
-      errorMessage: false,
-      message: "",
-      userid: ""
-    };
-  },
-  methods: {
-    async login() {
-      // Get input id
-      // let userId = this.input.userid;
-      let userId = this.userid;
+  export default {
+    name: "login",
+    components: {
+      NumericKeyboard
+    },
+    data() {
+      return {
+        // input: {
+        //   userid: ""
+        // },
+        errorMessage: false,
+        message: "",
+        userid: ""
+      };
+    },
+    methods: {
+      async login() {
+        // Get input id
+        // let userId = this.input.userid;
+        let userId = this.userid;
 
-      if (userId != "") {
-        // Get user data from db
-        let getUser;
-        await getUserById(userId).then(data => {
-          getUser = data;
-        });
+        if (userId != "") {
+          // Get user data from db
+          let getUser;
+          await getUserById(userId).then(data => {
+            getUser = data;
+          });
 
-        // Check if input id equals user id
-        if (userId == getUser.user_id) {
-          // Authorized
-          this.$store.dispatch("auth", true); // Set Auth Vuex state to true
+          // Check if input id equals user id
+          if (userId == getUser.user_id) {
+            // Authorized
+            this.$store.dispatch("auth", true); // Set Auth Vuex state to true
 
-          // Set Vuex user data
-          this.$store.dispatch("name", getUser.name);
-          this.$store.dispatch("type", getUser.user_type);
+            // Set Vuex user data
+            this.$store.dispatch("name", getUser.name);
+            this.$store.dispatch("type", getUser.user_type);
 
-          // Go to dashboard
-          this.$router.push("dashboard");
+            // Go to dashboard
+            this.$router.push("dashboard");
+          } else {
+            this.errorMessage = true;
+            this.message = "The user id is incorrect";
+          }
         } else {
           this.errorMessage = true;
-          this.message = "The user id is incorrect";
+          this.message = "Please, fill the user id";
         }
-      } else {
-        this.errorMessage = true;
-        this.message = "Please, fill the user id";
+      },
+
+      hideError() {
+        this.errorMessage = false
       }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-#login {
-  background: #293948;
-  height: 100vh;
-  width: 100%;
-  box-sizing: border-box;
-}
+  #login {
+    background: #293948;
+    height: 100vh;
+    width: 100%;
+    box-sizing: border-box;
+  }
 
-#inputLogIn {
-  -webkit-text-security: disc;
-  color: white;
-  text-align: center;
-  font-size: 5rem;
-}
+  #inputLogIn {
+    -webkit-text-security: disc;
+    color: white;
+    text-align: center;
+    font-size: 5rem;
+  }
 
-#keyboard {
-  margin: 2em;
-  width: 40%;
-  justify-content: center;
-}
+  #keyboard {
+    margin: 2em;
+    width: 40%;
+    justify-content: center;
+  }
 
-#logo {
-  padding: 1rem;
-  display: flex;
-  flex-wrap: wrap;
-  color: #ffffff;
-  flex-direction: column;
-  width: 50%;
-}
+  #logo {
+    padding: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    color: #ffffff;
+    flex-direction: column;
+    width: 50%;
+  }
 
-#loginInputContainer{
-  display: flex;
-  flex-flow: column;
-  height: 50%;
-}
+  #loginInputContainer {
+    display: flex;
+    flex-flow: column;
+    height: 50%;
+  }
+
 </style>
