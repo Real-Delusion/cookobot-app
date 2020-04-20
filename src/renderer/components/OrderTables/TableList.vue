412da3dfe8796a98f3a59cc3918cf7396fccbb3e
Santiago <sanpetor@epsg.upv.es>
2020-04-20 14:38:39 +0200

    updating style  while serving tables

--- src/renderer/components/OrderTables/TableList.vue
+++ src/renderer/components/OrderTables/TableList.vue
@@ -28,15 +28,19 @@
           v-bind:style="{ 'backgroundColor': 'white' }"
           ref="table"
         >
-          <font-awesome-icon class="draggable_icon" icon="grip-vertical" />
+          <font-awesome-icon 
+          :style="{'color':colorDraggableIcon}"
+          class="draggable_icon" icon="grip-vertical" />
           <p v-if="table!=0">Table {{ table }}</p>
           <p v-else>Kitchen</p>
           <div
             class="icon delete_icon"
+            :style="{ 'display': displayServing }"
+            ref="deleteTableIcon"
             @touchstart="deleteTable(table)"
             @mousedown="deleteTable(table)"
           >
-            <font-awesome-icon icon="times-circle" />
+            <font-awesome-icon icon="times-circle"/>
           </div>
         </SlickItem>
       </SlickList>
@@ -69,7 +73,7 @@
       <button
         class="button cancel_button is-danger is-fullwidth is-flex-tablet-only"
         type="button"
-        v-on:click="deleteAllTables()"
+        v-on:click="cancelServing()"
         :disabled="tables.length==0"
         v-else
       >
@@ -100,6 +104,9 @@ export default {
       tables: [],
       indexTables: 0,
       serving: false,
+      displayServing: "",
+      served: false,
+      colorDraggableIcon: ''
     };
   },
 
@@ -124,8 +131,12 @@ export default {
       // Changing style for the table that is serving
       let $refServing = this.$refs.table[this.indexTables].$el;
       $refServing.style.backgroundColor = "#ffff";
-      $refServing.style.border="solid";
-      $refServing.style.borderColor="var(--robot1)";
+      $refServing.style.border = "solid";
+      $refServing.style.borderColor = "var(--robot1)";
+      //this.displayServing = "none";
+      this.colorDraggableIcon= "white"
+
+      this.$refs.deleteTableIcon[this.indexTables].style.display='none'
 
       this.serving = true;
       //this.backgroundColor = "var(--disabled)";
@@ -133,14 +144,19 @@ export default {
       bus.$emit("sendTables", this.tables[this.indexTables]);
       bus.$on("sendRes", async res => {
         // Changing style for the table that is served
-          let $refServed = this.$refs.table[this.indexTables].$el;
-          $refServed.style.backgroundColor = "gray";
-          $refServed.style.color="gray"
+        let $refServed = this.$refs.table[this.indexTables].$el;
+        $refServing.style.border = "solid";
+        $refServing.style.borderColor = "var(--success)";
+  
+        this.served = true;
+        this.displayServing=''
+
         if (this.indexTables < this.tables.length - 1) {
           this.indexTables++;
           this.accept();
         } else {
-          console.log("FIN DE SERVIR MESAS");
+          console.log("END SERVING TABLES");
+          this.deleteAllTables();
           this.serving = false;
         }
       });
@@ -148,6 +164,11 @@ export default {
     deleteTable: function(table) {
       this.tables.splice(this.tables.indexOf(table), 1);
       bus.$emit("deleteTable", table);
+    },
+    cancelServing: function() {
+      bus.$emit("sendTables", -1);
+      this.serving = false;
+      this.tables=[];
     }
   }
 };
@@ -172,6 +193,8 @@ export default {
   font-size: 1.8rem;
   display: flex;
   align-items: center;
+  -webkit-user-drag:none;
+  user-select: none;
 }
 .draggable_icon {
   margin-right: 1ch;
