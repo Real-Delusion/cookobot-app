f95e4c40efa3726b89c08d3f88da5f0d18f99805
rousah <emvader@epsg.upv.es>
2020-04-20 14:00:58 +0200

    create robot list

---
+++ src/renderer/components/UI/ShowRobots/RobotButton.vue
@@ -0,0 +1,107 @@
+<template>
+  <div
+    class="robot-button"
+    v-bind:id="id"
+    v-bind:description="description"
+    @click="selectRobot(robot)"
+    v-on:click="changeColor($event)"
+  >
+    <div class="columns is-marginless">
+      <div class="column robot-icon is-two-fifths" v-bind:style="{backgroundColor: bgcolor}">
+        <span class="icon">
+          <font-awesome-icon icon="robot" />
+        </span>
+      </div>
+      <div class="column robot-text is-three-fifths">
+        <div>
+          <b>#{{id}}</b>
+        </div>
+        <div>{{description}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mixins: [],
+  props: ["id", "description", "bgcolor"],
+  data() {
+    return {
+      /* backgroundColor: "white",
+      buttonsEvents: []*/
+    };
+  },
+  created: async function() {
+    /*  bus.$on("deleteTables", tables => {
+      //if the list of tables is cancelled
+      if (tables == 0) {
+        this.backgroundColor = "white";
+      }
+    });
+    bus.$on("deleteTable", table => {
+      //if the table is deleted from the list
+      for (var i = 0; i < this.buttonsEvents.length; i++) {
+        if (this.buttonsEvents[i].value == table) {
+          this.buttonsEvents[i].style.backgroundColor = "white";
+          this.backgroundColor = "white";
+        }
+      }
+    });*/
+  },
+  methods: {
+    selectRobot: function(robot) {
+      //Send table value to TableList
+      // bus.$emit("robotSelected", robot);
+    },
+    changeColor: function(event) {
+      /* if (!this.buttonsEvents.includes(event.currentTarget)) {
+        this.buttonsEvents.push(event.currentTarget);
+      }
+
+      if (this.backgroundColor == "white") {
+        this.backgroundColor = "#00b7ff";
+      } else {
+        this.backgroundColor = "white";
+      }*/
+    }
+  }
+};
+</script>
+
+<style scoped>
+.robot-button {
+  height: 100%;
+  background: none;
+  border: none;
+  padding: 0;
+  width: 30vh;
+}
+
+.robot-icon {
+  background-color: var(--robot1);
+  border-radius: 0.5rem 0 0 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  color: white;
+}
+.robot-text {
+  background-color: white;
+  border-radius: 0 0.5rem 0.5rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  font-size: 1.5rem;
+  align-items: flex-start;
+  text-transform: capitalize;
+}
+.icon {
+  padding: 2.5rem;
+}
+.robot-text div {
+  height: auto;
+}
+</style>
---
+++ src/renderer/components/UI/ShowRobots/RobotList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="control robot-buttons">
+    <label class="radio" v-for="robot in robots" v-bind:key="robot.id">
+      <input type="radio" name="foobar" />
+      <RobotButton
+        v-bind:id="robot.id"
+        v-bind:description="robot.description"
+        v-bind:bgcolor="robot.color"
+        v-bind:style="{ 'margin-right': '1rem'}"
+      ></RobotButton>
+    </label>
+  </div>
+</template>
+
+<script>
+// import bus for events
+import RobotButton from "@/components/UI/ShowRobots/RobotButton";
+
+export default {
+  mixins: [],
+  components: { RobotButton },
+  data() {
+    return {
+      robots: [
+        { id: "123456", description: "kitchen", color: "var(--robot1)" },
+        { id: "789023", description: "terrace", color: "var(--robot2)" }
+      ]
+    };
+  },
+  created: async function() {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.robot-buttons {
+  padding: 1rem;
+  padding-top: 1.75rem;
+  padding-left: 0.75rem;
+}
+
+.radio input[type="radio"] {
+  opacity: 0;
+  position: fixed;
+  width: 0;
+}
+
+.radio input[type="radio"]:checked + div {
+  border: 0.5rem solid white;
+  border-radius: 0.7rem;
+  -webkit-box-shadow: 0px 0px 17px -8px rgba(0, 0, 0, 0.63);
+  -moz-box-shadow: 0px 0px 17px -8px rgba(0, 0, 0, 0.63);
+  box-shadow: 0px 0px 17px -8px rgba(0, 0, 0, 0.63);
+}
+</style>
