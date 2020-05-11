9106fb993ff1d58b2d698cafc067fd9c492214ba
Joan Ciprià <joancipria@gmail.com>
2020-05-11 22:54:19 +0200

    Implement move arm functions

--- src/renderer/mixins/ros.js
+++ src/renderer/mixins/ros.js
@@ -42,8 +42,10 @@ export default {
                         name: "navegacion_autonoma_servicio",
                         serviceType: "rossrv/Type"
                     });
-
                     console.log("Service created!");
+
+                    // Fold arm
+                    this.foldArm();
                 });
                 this.ros.on("error", error => {
                     console.log("Something went wrong when trying to connect");
@@ -55,20 +57,72 @@ export default {
                     if (this.connectionTries < 5) {
                         setTimeout(this.connectRos, 5000);
                         this.connectionTries++;
-                    }else{
+                    } else {
                         this.failed = true;
                     }
                 });
                 return resolve(0);
             });
         },
-        manualConnect(){
+        manualConnect() {
             this.failed = false;
-            this.connectionTries=0;
+            this.connectionTries = 0;
             this.connectRos();
         },
         disconnect: function () {
             this.ros.close();
+        },
+        moveArm: function (part, goal) {
+
+            // Get topic from part
+            switch (part) {
+                case "elbow":
+                    part = "arm_elbow_flex_controller";
+                    break;
+                case "shoulder":
+                    part = "arm_shoulder_lift_controller";
+                    break;
+                case "wrist":
+                    part = "arm_wrist_flex_controller";
+                    break;
+                default:
+                    break;
+            }
+
+            // Set topic
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/' + part + '/command',
+                messageType: 'std_msgs/Float64'
+            })
+
+            let timeSpace = 0;
+            if (Math.sign(goal) == 1) {
+                for (let i = 0; i < goal; i += 0.01) {
+                    timeSpace += 50;
+                    setTimeout(() => {
+                        let message = new ROSLIB.Message({
+                            data: i
+                        })
+                        topic.publish(message)
+                    }, timeSpace);
+                }
+            } else {
+                for (let i = 0; i > goal; i -= 0.01) {
+                    timeSpace += 50;
+                    setTimeout(() => {
+                        let message = new ROSLIB.Message({
+                            data: i
+                        })
+                        topic.publish(message)
+                    }, timeSpace);
+                }
+            }
+        },
+        foldArm: function () {
+            setTimeout(this.moveArm('elbow', -1), 0);
+            setTimeout(this.moveArm('shoulder', -1), 0);
+            setTimeout(this.moveArm('wrist', -1), 0);
         }
     }
 }
